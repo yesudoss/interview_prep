a80ff4642be51d25d51b85a9a1ec03175625ae10
Yesu Doss X <72723545+yesudoss@users.noreply.github.com>
2025-02-06 13:16:34 +0530

    Update bubble.py

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,13 +1,16 @@
-l = [2,1,3,4,5]
-n = len(l) - 1
-for i in range(n):
-    swapped = False
-    for j in range(0, n-i):
-        if l[j] > l[j+1]:
-            l[j], l[j+1] = l[j+1], l[j]
-            print(l)
-            swapped=True
-    if not swapped:
-        break
-print(l)
-    
+# bubble sort
+def bubble_sort(arr: list, n: int) -> list:
+    for i in range(0, n-1):
+        swapped=False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1], swapped = arr[j+1], arr[j], True
+        if not swapped:
+            break
+    return arr
+
+
+arr = [2,1,6,43,8,99,64,3]
+n = len(arr)
+result = bubble_sort(arr, n)
+print(result)
