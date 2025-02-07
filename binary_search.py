1fbabc0c34e751a7a977a549f19d70a6030e2837
Yesu Doss X <72723545+yesudoss@users.noreply.github.com>
2025-02-07 14:22:02 +0530

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(arr: list, target: int) -> int:
+    low = 0
+    high = len(arr) - 1
+    mid = low + (high-low) // 2 
+    while high>=low:
+        mid = low + (high-low) // 2 
+        if target == arr[mid]:
+            return mid
+        elif target > arr[mid]:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+    
+arr = [1,2,3,4,5,6,7]
+target = 77
+result = binary_search(arr, target)
+print(result)
